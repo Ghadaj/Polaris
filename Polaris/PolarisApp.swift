183bff27f62dc5ad69d440635e67b493ecd02871
GhadaJ <ghada.aljurbua@gmail.com>
2022-05-29 14:57:45 +0300

    Initial Commit

---
+++ Polaris/PolarisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarisApp.swift
+//  Polaris
+//
+//  Created by Ghada on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PolarisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
